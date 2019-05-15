fd292d31b8632de953ef1d9a0138e03dd0050c1a
Sven Bourree <sven.bourree@skynet.be>
2019-05-15 20:44:52 +0200

    Can place red tower on building spots but not blue ones for some reason

--- TowerDefenceEE/Assets/Scripts/TowerButton.cs
+++ TowerDefenceEE/Assets/Scripts/TowerButton.cs
@@ -14,15 +14,5 @@ public class TowerButton : MonoBehaviour
             return tower;
         }
     }
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+   
 }
--- TowerDefenceEE/Assets/Scripts/TowerManager.cs
+++ TowerDefenceEE/Assets/Scripts/TowerManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class TowerManager : Singleton<TowerManager>
 {
@@ -11,19 +12,43 @@ public class TowerManager : Singleton<TowerManager>
     public void selectedTower(TowerButton towerSelected)
     {
         towerButtonPressed = towerSelected;
-        Debug.Log(" you pressed" + towerButtonPressed.gameObject);
+        Debug.Log(" you pressed " + towerButtonPressed.gameObject);
     }
 
+    public void placeTower(RaycastHit2D hit)
+    {
+        //checks if you clic the UI (the buttons) because UI is not a game object
+        if (!EventSystem.current.IsPointerOverGameObject() && towerButtonPressed != null)
+        {
+            GameObject newTower = Instantiate(towerButtonPressed.Tower);
+            newTower.transform.position = hit.transform.position;
 
+
+        }
+    }
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // 0 is left mouse button 1 is right mouse button
+        if (Input.GetMouseButtonDown(0))
+        {
+            //this looks for the point on the map where you clicked
+            Vector2 mapPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            //vector2.zero is in the left bottom corner at position y:0 and x:0
+            RaycastHit2D hit = Physics2D.Raycast(mapPoint, Vector2.zero);
+            if (hit.collider.tag=="BuildSpot")
+            {
+                placeTower(hit);
+
+            }
+
+        }
+
     }
 }
